b7ff27cbd56fe9d7c47429df3e32f47c32332b8d
Anika Tahsin <56547691+anikatahsin50@users.noreply.github.com>
2021-12-01 13:03:26 +0600

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express()
 
 app.get('/',(req,res)=> {
-    res.send('Welcome azure')
+    res.send('Welcome azure anika')
 })
 
-app.listen(3000, ()=> {
-    console.log('Server is listening on port 3000....')
+app.listen(8080, ()=> {
+    console.log('Server is listening on port 8080....')
 })
